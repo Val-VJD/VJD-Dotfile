476e9ff1a1072ba5eda930fa176b2d1b120075f8
Val D <vald@vjdatc.com>
2025-10-30 21:41:41 -0400

    Bug fixes

--- dotfiles/.config/hypr/scripts/accentcolor.sh
+++ dotfiles/.config/hypr/scripts/accentcolor.sh
@@ -4,13 +4,30 @@
 
 wal -i $1
 
+# Copy the current wallpaper
+
+LINK="$HOME/.config/waypaper/current.png"
+
+[ -L "$LINK" ] && rm "$LINK"
+mkdir -p "$(dirname "$LINK")"
+cp "$1" "$LINK"
+
+# Copy a pywal css to UI element directory.
+
+copywalcss() {
+    DIR="$1"
+    CSSFILE="$HOME/.cache/wal/colors.css"
+
+    [ -L "$DIR/colors.css" ] && rm "$DIR/colors.css"
+    mkdir -p "$DIR"          # make sure the directory exists
+    cp "$CSSFILE" "$DIR/colors.css"
+}
+
+copywalcss "$HOME/.config/waybar/"
+copywalcss "$HOME/.config/swaync/"
+copywalcss "$HOME/.config/nwg-dock-hyprland/"
+copywalcss "$HOME/.config/wlogout/"
+
 # Restart UI elements
 
 ~/.config/hypr/scripts/uistartup.sh
-
-# Generate a symlink to the Wallpaper
-
-LINK=$HOME/.config/waypaper/current.png
-
-[ -L "$LINK" ] && rm "$LINK"
-ln -s "$1" "$LINK"
