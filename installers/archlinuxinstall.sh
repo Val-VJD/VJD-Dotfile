777ff5178b03c6a6076f4f9bd124accfe7384eee
Val-VJD <vdel71618@gmail.com>
2025-11-30 18:20:45 -0500

    Fixed packages installed via Official Repo

--- installers/archlinuxinstall.sh
+++ installers/archlinuxinstall.sh
@@ -19,7 +19,7 @@ install_pacman() {
     for pkg in "$@"; do
         if ! pacman -Qi "$pkg" &>/dev/null; then
             echo "Installing $pkg..."
-            if ! sudo pacman -S --needed --noconfirm "$pkg"; then
+            if ! sudo pacman -S --needed --noconfirm --overwrite "$pkg"; then
                 echo "Failed to install $pkg, skipping..."
             fi
         else
