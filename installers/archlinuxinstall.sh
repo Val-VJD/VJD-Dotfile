84d90e09a43127386f1785a37325f92f1613451b
Val-VJD <vdel71618@gmail.com>
2025-11-30 19:46:40 -0500

    ...

--- installers/archlinuxinstall.sh
+++ installers/archlinuxinstall.sh
@@ -70,7 +70,7 @@ cd ~/Graphite-gtk-theme && ./install.sh
 cd ~ && rm -rf ~/Graphite-gtk-theme
 
 # Install requests via pip
-pacman -S python-requests
+sudo pacman -S python-requests
 
 # Pre boot setup
 echo "Starting Setup"
